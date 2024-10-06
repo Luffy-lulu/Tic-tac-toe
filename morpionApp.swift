7e9699b02be023b96d312d61570e05ec4f33fde4
Luffy-lulu <146710770+Luffy-lulu@users.noreply.github.com>
2024-10-06 16:52:27 +0200

    pull

---
+++ morpionApp.swift
@@ -0,0 +1,17 @@
+//
+//  morpionApp.swift
+//  morpion
+//
+//  Created by Luca DELARUE 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct morpionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
